764291b34260a8c902d754fea5ae204923158c6f
sayakaliyah <116573858+sayakaliyah@users.noreply.github.com>
2023-02-19 00:12:10 +0800

    Profile7

--- week9/index.php
+++ week9/index.php
@@ -237,6 +237,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
         $password = "fancyR!ce36";
         $dbname = "webprogss211";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO sphernandez_myguests (id, name, email, website, comment, gender)
+VALUES ('$id', '$name, '$email', '$website', '$comment', '$gender')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 }
 
 ?>
